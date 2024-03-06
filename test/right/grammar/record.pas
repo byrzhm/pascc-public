a2ebcabe6ba80a819764e9f0fcaa046335daa7b3
zhm_mac <zhm1019@qq.com>
2024-03-06 15:32:00 +0800

    use Point record instead

--- test/right/grammar/record.pas
+++ test/right/grammar/record.pas
@@ -1,21 +1,15 @@
 program RecordExample;
 
 type
-    Student = record
-        name: string;
-        age: integer;
-        grade: real;
+    Point = record
+        x, y: Integer;
     end;
 
 var
-    student1: Student;
+    p: Point;
 
 begin
-    student1.name := 'John Doe';
-    student1.age := 20;
-    student1.grade := 85.5;
-
-    writeln('Student Name: ', student1.name);
-    writeln('Student Age: ', student1.age);
-    writeln('Student Grade: ', student1.grade);
+    p.x := 10;
+    p.y := 20;
+    WriteLn(p.x, ' ', p.y);
 end.
