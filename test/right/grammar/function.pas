83d30dd5879a0b2e24e675a5b3081259336ca06b
zhm_mac <zhm1019@qq.com>
2024-03-06 15:06:45 +0800

    update function Max

--- test/right/grammar/function.pas
+++ test/right/grammar/function.pas
@@ -4,14 +4,11 @@ var
     a, b: Integer;
 
 function Max(num1, num2: Integer): Integer;
-var
-    result: Integer;
 begin
     if (num1 > num2) then
-        result := num1
+        Max := num1
     else
-        result := num2;
-    Max := result;
+        Max := num2;
 end;
 
 begin
