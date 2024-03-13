f039b378613017dea2a7bedaa8e716c96da469f2
zhm_mac <zhm1019@qq.com>
2024-03-13 20:34:12 +0800

    Add test cases for scope and type errors

---
+++ test/wrong/type_error1.pas
@@ -0,0 +1,12 @@
+program TypeErrorExample;
+
+var
+    x: integer;
+
+begin
+    x := 10;
+    if x then
+        writeln('x is true')
+    else
+        writeln('x is false');
+end.
---
+++ test/wrong/type_error2.pas
@@ -0,0 +1,9 @@
+program TypeErrorExample;
+
+var
+    a: Integer;
+begin
+    a := 1.0;
+
+    writeln(a);
+end.
