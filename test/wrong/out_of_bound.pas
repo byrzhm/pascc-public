0eaf777f79a6fa52901440536d64cddae45d06c1
Bohuan Xia <254845576@qq.com>
2024-03-15 15:06:54 +0000

    add out_of_bound warning

---
+++ test/wrong/out_of_bound.pas
@@ -0,0 +1,11 @@
+program OutOfBound;
+
+var
+  arr: array[1..6] of integer;
+
+begin
+  //  Warning: range check error while evaluating 
+  //  constants (10 must be between 1 and 6)
+  
+  arr[10] := 1
+end.
