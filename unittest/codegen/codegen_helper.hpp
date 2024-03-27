f25deac614c6638369fcb6b757437a98a1198199
micropuma <micropuma@163.com>
2024-03-27 18:42:57 +0800

    support helloworld codegen test

--- unittest/codegen/codegen_helper.hpp
+++ unittest/codegen/codegen_helper.hpp
@@ -2,6 +2,24 @@
 
 #include "ast/ast.hpp"
 
+// Read expected code from file
+#include <fstream>
+
+auto read_expected(const std::string &filepath) -> std::string
+{
+  std::string expected_code;
+  std::ifstream file(filepath);
+  if (file.is_open()) {
+    std::string line;
+    while (std::getline(file, line)) {
+      expected_code += line += '\n';
+    }
+    file.close();
+  } else {
+    std::cout << "Failed to open file" << '\n';
+  }
+  return expected_code;
+}
 
 auto create_example_ast() -> std::unique_ptr<pascc::ast::Program>
 {
@@ -27,4 +45,21 @@ int main() {\n\
   printf(\"\\n\");\n\
 }\n";
   return code;
+}
+
+// construct program with writeln("Hello World");
+auto create_ast_helloworld() -> std::unique_ptr<pascc::ast::Program>
+{
+  std::unique_ptr<pascc::ast::Expr> actual = std::make_unique<pascc::ast::StringLiteral>("Hello World");
+  std::vector<std::unique_ptr<pascc::ast::Expr>> actuals;
+  actuals.emplace_back(std::move(actual));
+  std::unique_ptr<pascc::ast::Stmt> stmt = std::make_unique<pascc::ast::WritelnStmt>(std::move(actuals));
+  std::vector<std::unique_ptr<pascc::ast::Stmt>> stmts;
+  stmts.emplace_back(std::move(stmt));
+  std::unique_ptr<pascc::ast::CompoundStmt> compoundStmt = std::make_unique<pascc::ast::CompoundStmt>(std::move(stmts));
+  std::unique_ptr<pascc::ast::StmtPart> stmtPart         = std::make_unique<pascc::ast::StmtPart>(std::move(compoundStmt));
+  std::unique_ptr<pascc::ast::Block> blk                 = std::make_unique<pascc::ast::Block>(nullptr, nullptr, nullptr, nullptr, std::move(stmtPart));
+  std::unique_ptr<pascc::ast::ProgramBlock> progBlk      = std::make_unique<pascc::ast::ProgramBlock>(std::move(blk));
+  std::unique_ptr<pascc::ast::ProgramHead> progHead      = std::make_unique<pascc::ast::ProgramHead>("test");
+  return std::make_unique<pascc::ast::Program>(std::move(progHead), std::move(progBlk));
 }
--- unittest/codegen/codegen_test.cpp
+++ unittest/codegen/codegen_test.cpp
@@ -12,7 +12,21 @@ TEST(CodegenTest, example)
 
   std::string actual_code   = testing::internal::GetCapturedStdout();
 
-  std::string expected_code = get_example_expected_code();
+  std::string expected_code = read_expected("./data/codegen/1-basic.txt");
+
+  EXPECT_EQ(actual_code, expected_code);
+}
+
+TEST(CodegenTest, helloworld)
+{
+  testing::internal::CaptureStdout();
+  pascc::codegen::CodegenVisitor visitor("");
+  auto program = create_ast_helloworld();
+  program->accept(visitor);
+  std::string actual_code = testing::internal::GetCapturedStdout();
+
+  // get expected code sample from text.
+  std::string expected_code = read_expected("./data/codegen/2-helloworld.txt");
 
   EXPECT_EQ(actual_code, expected_code);
 }
