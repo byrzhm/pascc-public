4df0f0df4701089c5273e7c99a49a891aa32fa36
zhm_mac <zhm1019@qq.com>
2024-03-27 14:11:55 +0800

    bugfix: change create_example_ast

--- unittest/semant/semant_helper.hpp
+++ unittest/semant/semant_helper.hpp
@@ -5,8 +5,10 @@ auto create_example_ast() -> std::unique_ptr<pascc::ast::Program>
   std::unique_ptr<pascc::ast::Stmt> stmt = std::make_unique<pascc::ast::WritelnStmt>();
   std::vector<std::unique_ptr<pascc::ast::Stmt>> stmts;
   stmts.emplace_back(std::move(stmt));
-  std::unique_ptr<pascc::ast::StmtBlock> stmtBlk    = std::make_unique<pascc::ast::StmtBlock>(std::move(stmts));
-  std::unique_ptr<pascc::ast::ProgramBlock> progBlk = std::make_unique<pascc::ast::ProgramBlock>(std::move(*stmtBlk));
+  std::unique_ptr<pascc::ast::CompoundStmt> compoundStmt = std::make_unique<pascc::ast::CompoundStmt>(std::move(stmts));
+  std::unique_ptr<pascc::ast::StmtPart> stmtPart         = std::make_unique<pascc::ast::StmtPart>(std::move(compoundStmt));
+  std::unique_ptr<pascc::ast::Block> blk                 = std::make_unique<pascc::ast::Block>(nullptr, nullptr, nullptr, nullptr, std::move(stmtPart));
+  std::unique_ptr<pascc::ast::ProgramBlock> progBlk      = std::make_unique<pascc::ast::ProgramBlock>(std::move(blk));
   std::unique_ptr<pascc::ast::ProgramHead> progHead = std::make_unique<pascc::ast::ProgramHead>("test");
-  return std::make_unique<pascc::ast::Program>(std::move(*progHead), std::move(*progBlk));
+  return std::make_unique<pascc::ast::Program>(std::move(progHead), std::move(progBlk));
 }
