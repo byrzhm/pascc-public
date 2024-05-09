930bd740a42023fd07b486c933cca3d243435bca
zxb <2021211226@bupt.cn>
2024-05-09 08:34:53 +0000

    chore: add some commen ts

--- include/util/symtab.hpp
+++ include/util/symtab.hpp
@@ -5,6 +5,9 @@
 
 namespace pascc::util {
 
+/**
+ * @brief 符号表
+ */
 template<typename SYM, typename DAT>
 class SymTab
 {
@@ -13,16 +16,27 @@ public:
 
   /**
    * @brief 在当前作用域中查找符号
+   *
+   * @param name 符号名字
+   * @return 符号表中对应的数据
+   * @attention 在当前作用域
    */
   [[nodiscard]] auto probe(const SYM &name) const -> DAT;
 
   /**
    * @brief 在栈中存在的所有作用域中查找符号
+   *
+   * @param name 符号名字
+   * @return 符号表中对应的数据
+   * @attention 在所有作用域, 自内层作用域向外层作用域
    */
   [[nodiscard]] auto lookup(const SYM &name) const -> DAT;
 
   /**
    * @brief 在当前作用域中插入符号
+   *
+   * @param name 符号名字
+   * @param data 符号表中对应的数据
    */
   void insert(const SYM &name, const DAT &data);
 
--- include/util/type/type_comparator.hpp
+++ include/util/type/type_comparator.hpp
@@ -7,10 +7,24 @@ namespace pascc::util {
 class TypeComparator
 {
 public:
-  // TODO(): Implement this
+  /**
+   * @brief 比较两个类型是否相等
+   * 
+   * @param lhs 类型1
+   * @param rhs 类型2
+   * @return true 类型相同
+   * @return false 否则
+   */
   auto operator()(const SymType &lhs, const SymType &rhs) -> bool;
 
-  // TODO(): Implement this
+  /**
+   * @brief 比较两个类型是否可以转换，从from到to
+   * 
+   * @param from 类型1
+   * @param to 类型2
+   * @return true 可以转换，从from到to
+   * @return false 否则
+   */
   static auto cast(const SymType &from, const SymType &to) -> bool;
 };
 
